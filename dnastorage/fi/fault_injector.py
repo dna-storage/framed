7fcc1c2a2b230ccfaca2d9096c427f9606ffc291
kvolkel <kvolkel@ncsu.edu>
2018-08-13 11:27:33 -0400

    Add support for a combined fault injection

--- dnastorage/fi/fault_injector.py
+++ dnastorage/fi/fault_injector.py
@@ -43,7 +43,7 @@ class miss_strand(BaseModel):
         self._removal_sites=[]
         self._faulty_strands=[]
         self.remove_sites(self._removal_sites,self._input_library)
-        self._faulty_strands=self.remove_strands(self._removal_sites)
+        self._faulty_strands=self.remove_strands(self._removal_sites,self._input_library)
         self.write_out(self._faulty_strands)
 
         
@@ -56,9 +56,9 @@ class miss_strand(BaseModel):
             removal_sites.append(removal_strand_index)
 
  #remove the strands indicated by the sites list from the input library
-    def remove_strands(self,sites):
-        out_list=self._input_library[:]
-        #remove in reverse order so that we do not have to change the index of some sites after deleting
+    def remove_strands(self,sites,input_library):
+        out_list=input_library[:]
+        #remove in reversye order so that we do not have to change the index of some sites after deleting
         for strand_index in sorted(sites,reverse=True):
             del out_list[strand_index]
         return out_list
@@ -74,13 +74,13 @@ class strand_fault(BaseModel):
     def Run(self):
         self._injection={}
         self._error_strands=[]
-        self._injection=self.injection_sites()
-        self._error_strands=self.inject_errors(self._injection)
+        self._injection=self.injection_sites(self._input_library)
+        self._error_strands=self.inject_errors(self._injection,self._input_library)
         self.write_out(self._error_strands)
     #get strands to inject faults at and nucleotides within the strand
-    def injection_sites(self):
+    def injection_sites(self,input_library):
         #list of strand indexes to chose from 
-        strand_indexes=range(len(self._input_library))
+        strand_indexes=range(len(input_library))
         fault_list={} 
         strand_index_start=self._args.p1
         for strand_num in range(self._args.faulty):
@@ -88,7 +88,7 @@ class strand_fault(BaseModel):
             index,chosen_strand_index=self.random_array_element(strand_indexes)
             #delete chosen strands from list so that it is not chosen again
             del strand_indexes[index]
-            strand=self._input_library[chosen_strand_index]
+            strand=input_library[chosen_strand_index]
             
             fault_list[chosen_strand_index]={}
             #create list of nucleotide indexes so that we can avoid choosing the same index
@@ -109,8 +109,8 @@ class strand_fault(BaseModel):
         return fault_list
 
     #inject the errors into the selected strands and nucleotides
-    def inject_errors(self,inject_sites):
-        out_list=self._input_library
+    def inject_errors(self,inject_sites,input_library):
+        out_list=input_library[:]
         for strand_indexes in inject_sites:
             for fault_indexes in sorted(inject_sites[strand_indexes],reverse=True):
                 #substitution error
@@ -127,8 +127,25 @@ class strand_fault(BaseModel):
                     out_list[strand_indexes]=out_list[strand_indexes][0:fault_indexes]+insert_nucleotide+out_list[strand_indexes][fault_indexes:len(out_list[strand_indexes])]
                     
         return out_list
-                
 
+
+#This class implements a combination of missing strands and error strands 
+class combo(BaseModel):
+    def __init__(self,args):
+        BaseModel.__init__(self,args)
+        self.strand_faults=strand_fault(args)
+        self.missing_strands=miss_strand(args)
+        self.read_file()
+    def Run(self):
+        strands_after_missing=[]
+        missing_sites=[]
+        missing_strands_with_errors=[]
+        error_sites={}
+        self.missing_strands.remove_sites(missing_sites,self._input_library)
+        strands_after_missing=self.missing_strands.remove_strands(missing_sites,self._input_library)
+        error_sites=self.strand_faults.injection_sites(strands_after_missing)
+        missing_strands_with_errors=self.strand_faults.inject_errors(error_sites,strands_after_missing)
+        self.write_out(missing_strands_with_errors)
 
 
                     
