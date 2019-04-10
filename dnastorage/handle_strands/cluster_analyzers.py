73e12efe437a4368fe74abe394920e715032754a
kvolkel <kvolkel@ncsu.edu>
2019-04-09 22:51:22 -0400

    Add BMA cluster analysis

--- dnastorage/handle_strands/cluster_analyzers.py
+++ dnastorage/handle_strands/cluster_analyzers.py
@@ -2,6 +2,7 @@ from dnastorage.fi import fault_injector
 import Levenshtein as lv
 import numpy as np
 import matplotlib.pyplot as plt
+import operator
 #this cluster analysis technique tries to come up with a way to extract the correct strand using the context of all the other strands in the cluster. The idea is to try to understand what edits need to be done to transform a strand to the original strand.
 def cluster_analyze_ed(strand_cluster,rate,expected_length,iterations):
 
@@ -53,11 +54,64 @@ def cluster_analyze_ed(strand_cluster,rate,expected_length,iterations):
         _strand_cluster=list(_strand_cluster_new)
 
 
+def cluster_analyze_majority(strand_cluster,strand_length):
+    final_strand=[]
+    pointer_list=[0]*len(strand_cluster)
+    majority_vote={"A":0,"G":0,"C":0,"T":0}
+    fix_error={"del":0,"inser":0,"sub":0}
+    
+    for final_strand_index in range(0,strand_length):
+        majority_vote["A"]=0
+        majority_vote["G"]=0
+        majority_vote["C"]=0
+        majority_vote["T"]=0
+        for cluster_strand_index, cluster_strand in enumerate(strand_cluster):
+            majority_vote[cluster_strand[pointer_list[cluster_strand_index]]]+=1 #sum up across all the current pointers
+
+        #apply the majority vote
+        majority_base=max(majority_vote.iteritems(), key=operator.itemgetter(1))[0];
+        final_strand.append(majority_base)
+        for cluster_strand_index, cluster_strand in enumerate(strand_cluster):
+            if cluster_strand[pointer_list[cluster_strand_index]]!=majority_base:
+                fix_error["del"]=0
+                fix_error["inser"]=0
+                fix_error["sub"]=0
+                for cluster_strand_index_fix, cluster_strand_fix in enumerate(strand_cluster):
+                    sub_index=min(pointer_list[cluster_strand_index]+1,len(cluster_strand)-1)
+                    sub_index_fix=min(pointer_list[cluster_strand_index_fix]+1,len(cluster_strand_fix)-1)
+                    del_index=pointer_list[cluster_strand_index]
+                    del_index_fix=min(pointer_list[cluster_strand_index_fix]+1,len(cluster_strand_fix)-1)
+                    insert_index=min(pointer_list[cluster_strand_index]+1,len(cluster_strand)-1)
+                    insert_index_fix=pointer_list[cluster_strand_index_fix]
+                    if cluster_strand_fix[pointer_list[cluster_strand_index_fix]]==majority_base:
+                        if cluster_strand[sub_index]==cluster_strand_fix[sub_index_fix]:
+                            fix_error["sub"]+=1
+                        elif cluster_strand[del_index]==cluster_strand_fix[del_index_fix]:
+                            fix_error["del"]+=1
+                        elif cluster_strand[insert_index]==cluster_strand_fix[insert_index_fix]:
+                            fix_error["inser"]+=1
+                majority_fix=max(fix_error.iteritems(), key=operator.itemgetter(1))[0]
+                if majority_fix=="inser":
+                    pointer_list[cluster_strand_index]=min(len(cluster_strand)-1,pointer_list[cluster_strand_index]+2)
+                elif majority_fix=="sub":
+                    pointer_list[cluster_strand_index]=min(len(cluster_strand)-1,pointer_list[cluster_strand_index]+1)
+            else:
+                #just increment if it is in the majority
+                pointer_list[cluster_strand_index]=min(len(cluster_strand)-1,pointer_list[cluster_strand_index]+1)
+    return "".join(final_strand)
 
 
+        
 if __name__=="__main__":
     base_strand="ATCCTACTCCAGCGGGATCTTTTATCTAAAGACGATGAGAGGAGTATTCGTCAGGCCACATAGCTTTCATGTTCTGATCGGAACGATCGTTGGCGCCCGACCCTCGGATTCCGTAGTGAGTTCTTTGTCCGAGCCATTGTATGCGAGATCGGTAGACTGATAGGGGATGCAGTATATCCCTGGATGCAATAGACGGACAG"
 
+    import argparse
+    parser = argparse.ArgumentParser(description="Analyze different cluster analysis methods")
+    parser.add_argument('--analysis_type', dest="cluster_type",action="store", default=None, help='type of analysis to perform')
+
+
+    args = parser.parse_args()
+    
     injector_args=fault_injector.fault_injector_arguments()
     injector_args.o="outfile.log"
     injector_args.input_file=None
@@ -71,7 +125,7 @@ if __name__=="__main__":
     fault_model=eval('fault_injector.'+"fixed_rate"+"(injector_args)")
     data_set={}
     
-    for strand_count_step in range(30,35,5):
+    for strand_count_step in range(60,80,5):
         if strand_count_step not in data_set:
             data_set[str(strand_count_step)+"_strands"]=[]
         base_set=[base_strand for i in range(0,strand_count_step)]
@@ -86,7 +140,10 @@ if __name__=="__main__":
                 error_set=fault_model.Run()
                 distance_before=lv.distance(error_set[0],base_strand)
                 distance_before_array.append(distance_before)
-                return_strand=cluster_analyze_ed(error_set,rate_step,200,1)
+                if args.cluster_type=="ed":
+                    return_strand=cluster_analyze_ed(error_set,rate_step,200,1)
+                elif args.cluster_type=="BMA":
+                    return_strand=cluster_analyze_majority(error_set,200)
                 distance_after=lv.distance(return_strand,base_strand)
                 if distance_after==0:
                     correct_count+=1
@@ -113,10 +170,3 @@ if __name__=="__main__":
 
 
 
-    '''
-    print "edit distance after analysis: {}\n".format(lv.distance(return_strand,base_strand))
-    print "edit distance before analysis {}\n".format(lv.distance(error_set[0],base_strand))
-    print "strand before error :\n {}\n".format(error_set[0])
-    print "strand original :\n {}\n".format(base_strand)
-    print "returned strand :\n {}\n".format(return_strand)
-    '''
