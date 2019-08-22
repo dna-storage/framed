0972c06256bee1e31b84144bf7faefdc07c2786b
kvolkel <kvolkel@ncsu.edu>
2019-08-22 09:56:27 -0400

    Improve support for launching LSF FI jobs

--- tools/lsf/launch_fi.sh
+++ tools/lsf/launch_fi.sh
@@ -1,9 +1,15 @@
 #wrapper script for launching fault injection campaigns across many configurations
 
+python command_generation_fi.py --config $1 #generates the command lines from a config file
 
 
+jobCounter=0
 while read line; do
+    echo $jobCounter
+    echo $line
+    bsub -W 10000 -N -C 0 -n 12 -o $HOME/dna_fi_dumps/stdout_benchmark_$jobCounter.txt -e $HOME/dna_fi_dumps/sterr_benchmark_$jobCounter.txt -q long \
+	 -P DNA_FI -R "select[hc] rusage[mem=8000]" python fault_injection.py  $line
+    jobCounter=$((jobCounter+1))
+done < intermediate.commands
 
-     bsub -W 100 -N -C 0 -n 12 -o $HOME/dna_fi_dumps/stdout_benchmark.txt -e $HOME/dna_fi_dumps/sterr_benchmark.txt -q standard -P DNA_FI -R "select[hc] rusage[mem=8000]"  $line
-
-done < $1 
+rm intermediate.commands
